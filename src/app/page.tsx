9fe7895ea55e84e06f5e974e3747750ea93c398c
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2024-01-15 11:29:22 -0600

    fixed error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,11 @@ import Image from 'next/image'
 import BackgroundTogler from '../components/BackgroundTogler'
 
 export default function Home() {
-  function toggleButton() {
-    // Add your toggle logic here if needed
-    console.log("Button toggled!");
-  }
   return (
-    <div className='max-w-[20px]'>
-     <input type="checkbox" onClick={toggleButton()}/>
-      <span className="slider"></span>
+    <div>
+      <div >
+        <BackgroundTogler/>
+      </div>
     </div>
   )
 }
