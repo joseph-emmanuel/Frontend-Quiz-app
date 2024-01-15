09b05d0b586f45add0139e70f63fe6752f2e64a2
Joseph Emmanuel <joseph.emmanuel@greatgulf.com>
2024-01-15 13:08:29 -0500

    added toggle

--- src/components/BackgroundTogler/index.js
+++ src/components/BackgroundTogler/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function index() {
+const Switch = () => {
+  const [isChecked, setChecked] = useState(false);
+
+  const toggleSwitch = () => {
+    setChecked(!isChecked);
+    console.log("isChecked", !isChecked)
+  };
+
   return (
-    <div>index</div>
-  )
-}
+    <div className="switch-container">
+      <label className={`switch ${isChecked ? 'checked' : ''}`}>
+        <input type="checkbox" checked={isChecked} onChange={toggleSwitch} />
+        <span className="slider"></span>
+      </label>
+    </div>
+  );
+};
 
-export default index
+export default Switch;
